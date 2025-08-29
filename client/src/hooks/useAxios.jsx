539d716c6fcad634166f469dfb2576224be05554
Shanto <mrahman7148@gmail.com>
2025-08-29 18:38:55 +0600

    deploy

--- client/src/hooks/useAxios.jsx
+++ client/src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://zap-shift-3.onrender.com`
 })
 
 const useAxios = () => {
--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import {useNavigate} from 'react-router';
 
 
 const axiosSecure = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://zap-shift-3.onrender.com`
 });
 
 const useAxiosSecure = () => {
